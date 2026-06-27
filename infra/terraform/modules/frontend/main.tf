b830ab26846afd34380ba28cc41917f0d7d173fa
madanfd <madan.chaudhary@fanduel.com>
2026-06-27 12:47:37 +0100

    terraform fix

--- infra/terraform/modules/frontend/main.tf
+++ infra/terraform/modules/frontend/main.tf
@@ -19,8 +19,6 @@ locals {
 
   # AWS managed cache policy: Managed-CachingOptimized.
   cloudfront_cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
-  # AWS managed cache policy: Managed-CachingDisabled.
-  cloudfront_api_cache_policy_id = "4135ea2d-6df8-44a3-9df8-4b5a84be39ad"
 }
 
 data "aws_caller_identity" "current" {}
@@ -92,6 +90,33 @@ resource "aws_cloudfront_origin_request_policy" "backend_api" {
   }
 }
 
+resource "aws_cloudfront_cache_policy" "backend_api" {
+  count = local.api_origin_enabled ? 1 : 0
+
+  name        = "${local.name}-backend-api-cache"
+  comment     = "Disable caching for ${local.name} backend API requests"
+  default_ttl = 0
+  max_ttl     = 0
+  min_ttl     = 0
+
+  parameters_in_cache_key_and_forwarded_to_origin {
+    enable_accept_encoding_brotli = false
+    enable_accept_encoding_gzip   = false
+
+    cookies_config {
+      cookie_behavior = "none"
+    }
+
+    headers_config {
+      header_behavior = "none"
+    }
+
+    query_strings_config {
+      query_string_behavior = "none"
+    }
+  }
+}
+
 resource "aws_cloudfront_distribution" "frontend" {
   enabled             = true
   is_ipv6_enabled     = true
@@ -137,7 +162,7 @@ resource "aws_cloudfront_distribution" "frontend" {
     content {
       allowed_methods          = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
       cached_methods           = ["GET", "HEAD", "OPTIONS"]
-      cache_policy_id          = local.cloudfront_api_cache_policy_id
+      cache_policy_id          = aws_cloudfront_cache_policy.backend_api[0].id
       compress                 = true
       origin_request_policy_id = aws_cloudfront_origin_request_policy.backend_api[0].id
       path_pattern             = "/api/*"
